4f40ddd60c0abbe94e9857d05552c04f39b3c3cb
Tetiana343 <emilianovolk75@gmail.com>
2024-04-22 17:59:24 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Week_4_Day_6_Lists_key_topic
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Custom error.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4_Day_6_Lists_key_topic
+{
+    internal class Custom_error
+    {
+
+        //fields
+        //Error code-message
+
+        string _errorCode;
+        string _errorMessage;
+
+
+
+        //constructors
+
+        public Custom_error custom_Error { get; set; }
+
+        //properties
+        public string ErrorCode { get => _errorCode; }
+
+        public string ErrorMessage { get => _errorMessage;}
+
+        
+
+        
+    }//main
+
+}// namespace
